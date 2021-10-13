0f1aa922f944fe93b23fffc3692f2b989cdc4912
AniketRane <aniketrane24.11@gmail.com>
2021-10-13 11:22:17 +0530

    Iframe's css changes

--- index.js
+++ index.js
@@ -43,6 +43,8 @@ class CSVBoxImporter {
                 "height: 100%;" +
                 "width: 100%;" +
                 "position: absolute;" +
+                "top: 0px;" +
+                "left: 0px;" +
             "}";
 
         let css = document.createElement("style");
