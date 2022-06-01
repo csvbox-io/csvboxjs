0824fa9eb1d467f99452827cc9c75d409faf2a80
AniketRane <aniketrane24.11@gmail.com>
2022-06-01 17:25:52 +0530

    Update

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 class CSVBoxImporter {
-            
-    constructor(slug, data = {}, callback = null) {
-        
+
+    constructor(slug, data = {}, callback = null, configuration = {}) {
+
         this.isIframeLoaded = false;
         this.shouldOpenModalonIframeLoad = false;
         this.slug = slug;
         this.data = data;
+
         this.columns = [];
         this.options = [];
+
+        this.configuration = configuration;
 
         if (callback && (typeof callback == "function")) {
             this.callback = callback;
         }
-        let self= this;
+
+        let self = this;
+
         if(document.readyState === "complete") {
             self.setUpImporter();
         }
+
         document.addEventListener('DOMContentLoaded', function() {
             self.setUpImporter();
         });
@@ -44,8 +50,6 @@ class CSVBoxImporter {
                 "height: 100%;" +
                 "width: 100%;" +
                 "position: absolute;" +
-                "top: 0px;" +
-                "left: 0px;" +
             "}";
 
         let css = document.createElement("style");
@@ -63,8 +67,15 @@ class CSVBoxImporter {
 
         let iframe = document.createElement("iframe");
         this.iframe = iframe;
-        
-        iframe.setAttribute("src", "https://app.csvbox.io/embed/" + this.slug);
+
+        let BASE_URL = "https://app.csvbox.io/embed/";
+
+        let url = BASE_URL + this.slug;
+        url += `?debug=${!!this.configuration?.debug}`;
+        url += `&source=embedCode`;
+
+        iframe.setAttribute("src", url);
+
         iframe.frameBorder = 0;
         this.holder.id = this.id;
         this.holder.appendChild(iframe);
@@ -73,32 +84,28 @@ class CSVBoxImporter {
         let self = this;
 
         iframe.onload = function () {
+
             self.isIframeLoaded = true;
+
             if(self.shouldOpenModalonIframeLoad) {
                 self.shouldOpenModalonIframeLoad = false;
                 self.openModal();
             }
+
             iframe.contentWindow.postMessage({
                 "customer" : self.data
             }, "*");
+
             iframe.contentWindow.postMessage({
                 "columns" : self.columns
             }, "*");
+
             iframe.contentWindow.postMessage({
                 "options" : self.options
             }, "*");
-        }
 
-        if(document.querySelector("[data-csvbox]") != null){
-            document.onreadystatechange = () => {
-                if (document.readyState === 'complete') {
-                    document.querySelector("[data-csvbox]").disabled = false;
-                }else{
-                    document.querySelector("[data-csvbox]").disabled = true;
-                }
-            };
+            self.onReady?.();
         }
-        
     }
 
     setUser(data) {
@@ -110,11 +117,26 @@ class CSVBoxImporter {
         }
     }
 
+    listen(type, callback = null) {
+        if(typeof callback == "function") {
+            switch(type){
+                case "onReady":
+                    this.onReady = callback;
+                    break;
+                case "onClose":
+                    this.onClose = callback;
+                    break;
+            }
+        }
+    }
+
     setupMessageListener() {
         window.addEventListener("message", (event) => {
+
             if (event.data === "mainModalHidden") {
                 this.holder.style.display = 'none';
                 this.isModalShown = false;
+                this.onClose?.();
             }
             if(event.data === "uploadSuccessful") {
                 if(this.callback && (typeof this.callback == "function")){
@@ -128,12 +150,48 @@ class CSVBoxImporter {
             }
             if(typeof event.data == "object") {
                 if(event.data.type && event.data.type == "data-push-status") {
-                    if(event.data.data.import_status == "success"){
-                        this.callback(true, event.data.data);
-                    }else {
-                        this.callback(false, event.data.data);
-                    }
+                    if(event.data.data.import_status == "success") {
+                        // this.callback(true, event.data.data);
+                        if(event?.data?.row_data) {
+                            let primary_row_data = event.data.row_data;
+                            let headers = event.data.headers;
+                            let rows = [];
+                            let dynamic_columns_indexes = event.data.dynamicColumnsIndexes;
+                            primary_row_data.forEach((row_data) => {
+                                let x = {};
+                                let dynamic_columns = {};
+                                row_data.data.forEach((col, i)=>{
 
+                                    if(col == undefined){ col = ""};
+
+                                    if(dynamic_columns_indexes.includes(i)) {
+                                        dynamic_columns[headers[i]] = col;
+                                    }else{
+                                        x[headers[i]] = col;
+                                    }
+                                });
+                                if(row_data?.unmapped_data) {
+                                    x["_unmapped_data"] = row_data.unmapped_data;
+                                }
+                                if(dynamic_columns && Object.keys(dynamic_columns).length > 0) {
+                                    x["_dynamic_data"] = dynamic_columns;
+                                }
+                                rows.push(x);
+                            });
+                            let metadata = event.data.data;
+                            metadata["rows"] = rows;
+                            delete metadata["unique_token"];
+                            this.callback(true, metadata);
+                        }else{
+                            let metadata = event.data.data;
+                            delete metadata["unique_token"];
+                            this.callback(true, metadata);
+                        }
+                    }else {
+                        let metadata = event.data.data;
+                        delete metadata["unique_token"];
+                        this.callback(false, metadata);
+                    }
                 }
             }
         }, false);
@@ -149,6 +207,7 @@ class CSVBoxImporter {
         }else{
             this.shouldOpenModalonIframeLoad = true;
         }
+
     }
 
     randomString() {
@@ -179,6 +238,16 @@ class CSVBoxImporter {
         }
     }
 
+}
+
+if(document.querySelector("[data-csvbox]") != null){
+    document.onreadystatechange = () => {
+        if (document.readyState === 'complete') {
+            document.querySelector("[data-csvbox]").disabled = false;
+        }else{
+            document.querySelector("[data-csvbox]").disabled = true;
+        }
+    };
 }
 
 export default CSVBoxImporter;
